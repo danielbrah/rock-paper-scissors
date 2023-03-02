a719a220341554fa22977c59b7d84354a003720e
Daniel <daniel.jimenezfrancisco@gmail.com>
2023-03-02 11:54:37 -0600

    Improving script.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,20 @@ const closeBtn = document.querySelectorAll('.close-modal')
 const modal = document.getElementById('rules-modal')
 const overlay = document.getElementById('overlay')
 
+const closeModal = function() 
+{
+    modal.classList.remove('active')
+    overlay.classList.remove('active') 
+}
+
 closeBtn.forEach(element =>{
-    element.addEventListener('click', () =>{
-        modal.classList.remove('active')
-        overlay.classList.remove('active')
-    })
+    element.addEventListener('click', closeModal)
+})
+
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', e => {
+    if(e.key == 'Escape' && modal.classList.contains('active')) closeModal()
 })
 
 modalBtn.addEventListener('click', () => {
