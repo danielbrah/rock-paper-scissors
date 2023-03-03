ac6eb28815b96ce24a33514b2df58dd90bf6f9c0
Daniel <daniel.jimenezfrancisco@gmail.com>
2023-03-03 12:40:46 -0600

    Changes. Still working on game functions.

--- js/script.js
+++ js/script.js
@@ -2,11 +2,29 @@ const modalBtn = document.getElementById('modal__btn')
 const closeBtn = document.querySelectorAll('.close-modal')
 const modal = document.getElementById('rules-modal')
 const overlay = document.getElementById('overlay')
+const choice = document.querySelectorAll('.choice')
 
 const closeModal = function() 
 {
     modal.classList.remove('active')
     overlay.classList.remove('active') 
+}
+
+const checkChoice = function(e) {
+    switch(e.getAttribute('id'))
+    {
+        case 'rock':
+            return 'Rock!'
+
+        case 'paper':
+            return 'Paper!'
+
+        case 'scissors':
+            return 'Scissors!'
+        
+        default:
+            return 'Error!'
+    }
 }
 
 closeBtn.forEach(element =>{
@@ -22,4 +40,12 @@ document.addEventListener('keydown', e => {
 modalBtn.addEventListener('click', () => {
     modal.classList.toggle('active')
     overlay.classList.toggle('active')
+})
+
+choice.forEach(element => {
+    element.addEventListener('click', () => {
+        console.log(checkChoice(element))
+        document.getElementById('game__phase__1').classList.toggle('hidden')
+        document.getElementById('game__phase__2').classList.toggle('show')
+    })
 })
