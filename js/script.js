b077b82a6f2d72a2a4fc535a735f464cd3448f06
Daniel <daniel.jimenezfrancisco@gmail.com>
2023-03-07 18:12:59 -0600

    Update.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const closeBtn = document.querySelectorAll('.close-modal')
 const modal = document.getElementById('rules-modal')
 const overlay = document.getElementById('overlay')
 const choice = document.querySelectorAll('.choice')
+const yourChoice = document.getElementById('your-choice')
 
 const closeModal = function() 
 {
@@ -10,16 +11,42 @@ const closeModal = function()
     overlay.classList.remove('active') 
 }
 
+const setImage = function(choice)
+{
+    // Creating element
+    const img = document.createElement('img')
+    const playerChoice = document.createElement('div')
+    const bg = document.createElement('div')
+    const text = document.createElement('p')
+
+    // Setting image source and div classnames
+    bg.classList.add('choice__icon__bg')
+    playerChoice.classList.add('choice__icon')
+    playerChoice.classList.add(`${choice}-icon`)
+    playerChoice.style.position = 'relative'
+    img.src = `images/icon-${choice}.svg`
+    text.textContent = 'Your Choice'
+
+    // Appending elements
+    playerChoice.appendChild(img)
+    playerChoice.appendChild(bg)
+    yourChoice.appendChild(playerChoice)
+    yourChoice.appendChild(text)
+}
+
 const checkChoice = function(e) {
     switch(e.getAttribute('id'))
     {
         case 'rock':
+            setImage('rock')
             return 'Rock!'
 
         case 'paper':
+            setImage('paper')
             return 'Paper!'
 
         case 'scissors':
+            setImage('scissors')
             return 'Scissors!'
         
         default:
